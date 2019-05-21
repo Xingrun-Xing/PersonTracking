7a1fe6ad29f58b9d8f33fd126e1f54dd5dbc3a2e
Xingrun-Xing <48146545+Xingrun-Xing@users.noreply.github.com>
2019-05-21 19:10:30 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,28 @@
+#include<opencv2/opencv.hpp>
+#include"PersonTracking.hpp"
+using namespace std;
+using namespace cv;
+#define devView(i) imshow(#i,i)
+
+int main()
+{
+	PersonTracking person;
+	person.KillDCM();
+	person.Init();
+	for (; waitKey(1) != 27; )
+	{
+		Mat show = person.QueryColorImage();
+		if (person.Update())
+		{
+			auto Tperson = person.Queryperson();
+			cout << person.QueryBoundingBox(Tperson) << endl;
+			show = person.draw(Tperson);
+			cout << person.QueryMassCenterWorld(Tperson).x << "  "
+				<< person.QueryMassCenterWorld(Tperson).y << "   "
+				<< person.QueryMassCenterWorld(Tperson).z << endl;
+		}
+		devView(show);
+		person.release();
+	}
+	return 0;
+}
